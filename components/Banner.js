f131784b92ed414e32df8b772022e20a2642e972
Mellow <justfansof@gmail.com>
2023-11-14 16:21:10 -0500

    Banner Start up and about page framing setup

--- components/Banner.js
+++ components/Banner.js
@@ -1,49 +1,16 @@
-export default function Text(props) {
+import styles from './Banner.module.css'
+
+import Text from '@components/Text'
+
+
+export default function Banner(props) {
     return (
-      <>
-        {
-            (() => {
-                if(props.type === "h1") {
-                    return (
-                        <h1 className={"text-xl text-white text-center md:text-center md:text-xxxl" + " " + (props.className)}>{props.text}</h1>
-                    )
-                } else if (props.type === "h2") {
-                    return (
-                        <h2 className={"text-xxl font-black md:text-xxl" + " " + (props.className)}>{props.text}</h2>
-                    ) 
-                } else if (props.type === "h3") {
-                    return (
-                        <h3 className={"text-xl text-white md:text-xxl" + " " + (props.className)}>{props.text}</h3>
-                    ) 
-                } else if (props.type === "h4") {
-                    return (
-                        <h4 className={"text-lg font-black text-center" + " " + (props.className)}>{props.text}</h4>
-                    ) 
-                } else if (props.type === "h5") {
-                    return (
-                        <h5 className={"text-base text-white" + " " + (props.className)}>{props.text}</h5>
-                    ) 
-                } else if (props.type === "h6") {
-                    return (
-                        <h6>{props.text}</h6>
-                    ) 
-                }   else if (props.type === "p") {
-                    return (
-                        <p className={"py-4" + " " + (props.className)}>{props.text}</p>
-                    ) 
-                } else if (props.type === "a") {
-                    return (
-                        <a href={(props.link)} className={"py-4" + " " + (props.className)}>{props.text}</a>
-                    ) 
-                } else {
-                        return (
-                            "ERR"
-                        )
-                    }
-                }
-            )() 
-        }
-      </>
+        <div className={styles.banner}>
+            <Text 
+                type={props.textType}
+                text={props.text}
+            />
+        </div>
     )
-  }
+}
   
--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import Footer from "@components/Footer";
 import Menu from "@components/Menu";
 import ExtraMenu from "@components/ExtraMenu"
 
+import Banner from '@components/Banner'
 import Text from '@components/Text';
 
 
@@ -24,13 +25,19 @@ export default function Home() {
         </div>
         <div className="main-section">
           <div className="main-internal flex flex-row">
-            <div className="portrait-section flex-column">
+            <div className="portrait-section flex flex-col justify-center align-center">
               {/* Self Portrait */}
-              <img src="https://placehold.co/300x300"/>
+              <img src="https://placehold.co/150x150"/>
               {/* Self Portrait Banner */ }
               <Text 
                 type="h3"
                 text="Adam Elsedoudi"
+              />
+            </div>
+            <div className="informational-section">
+              <Banner 
+                textType="h1"
+                text="About Me"
               />
             </div>
           </div>
